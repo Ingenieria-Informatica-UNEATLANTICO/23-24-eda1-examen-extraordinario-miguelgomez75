42b1f99ffebfffbed0d98fa27e5102aa750e76c2
miguelgomez75 <miguel.gomez@alumnos.uneatlantico.es>
2024-07-05 15:11:47 +0200

    Se crea el método de agregar un nodo en base a su posición

--- src/ListaHamburguesa.java
+++ src/ListaHamburguesa.java
@@ -3,4 +3,25 @@ package src;
 public class ListaHamburguesa {
     private Nodo Cabeza;
     private int numeroingredientes;
+
+    public ListaHamburguesa() {
+        this.Cabeza = null;
+        this.numeroingredientes = 0;
+    }
+
+    public void agregar(Object data, int posicion) {
+        Nodo nuevoNodo = new Nodo(data);
+        if (posicion == 0) {
+            nuevoNodo.setNext(Cabeza);
+            Cabeza = nuevoNodo;
+        } else {
+            Nodo actual = Cabeza;
+            for (int i = 0; i < posicion - 1; i++) {
+                actual = actual.getNext();
+            }
+            nuevoNodo.setNext(actual.getNext());
+            actual.setNext(nuevoNodo);
+        }
+        numeroingredientes++;
+    }
 }
