d38fcfb3caf312d1c2bbb88e2d89940d8f8cfbf7
miguelgomez75 <miguel.gomez@alumnos.uneatlantico.es>
2024-07-05 15:22:55 +0200

    Se añade un método para intercambiar de posición dos ingredientes de la hamburguesa

--- src/ListaHamburguesa.java
+++ src/ListaHamburguesa.java
@@ -37,4 +37,42 @@ public class ListaHamburguesa {
         }
         numeroingredientes--;
     }
+
+    public void reorganizar(int posicion1, int posicion2) {
+        if (posicion1 > posicion2) {
+            int temp = posicion1;
+            posicion1 = posicion2;
+            posicion2 = temp;
+        }
+
+        Nodo nodo1 = Cabeza;
+        Nodo prevNodo1 = null;
+        for (int i = 0; i < posicion1; i++) {
+            prevNodo1 = nodo1;
+            nodo1 = nodo1.getNext();
+        }
+
+        Nodo nodo2 = Cabeza;
+        Nodo prevNodo2 = null;
+        for (int i = 0; i < posicion2; i++) {
+            prevNodo2 = nodo2;
+            nodo2 = nodo2.getNext();
+        }
+
+        if (prevNodo1 != null) {
+            prevNodo1.setNext(nodo2);
+        } else {
+            Cabeza = nodo2;
+        }
+
+        if (prevNodo2 != null) {
+            prevNodo2.setNext(nodo1);
+        } else {
+            Cabeza = nodo1;
+        }
+
+        Nodo temp = nodo1.getNext();
+        nodo1.setNext(nodo2.getNext());
+        nodo2.setNext(temp);
+    }
 }
